ba046837575fa37ba70e5e76722e6031c6217679
treyshedrick <treyshedrick@gmail.com>
2021-03-05 13:37:55 -0600

    change iframe size / src on loaded screen size

---
+++ iframe.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    let smallsrc = "https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FHoustonSpaceCityzens%2F&tabs=timeline&width=300&height=400&small_header=false&adapt_container_width=true&hide_cover=true&show_facepile=true&appId"
+    let largesrc = "https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FHoustonSpaceCityzens%2F&tabs=timeline&width=500&height=280&small_header=true&adapt_container_width=true&hide_cover=true&show_facepile=true&appId"
+        if(window.outerWidth < 550){
+            $('#iframesrc').attr('src', smallsrc);
+        } else{
+            $('#iframesrc').attr('src', largesrc);
+        }
+    console.log(window.outerWidth)
+});
