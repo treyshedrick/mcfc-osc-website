0ef6d119e25cd67a44f05c64237f99049241b803
treyshedrick <treyshedrick@gmail.com>
2021-03-11 19:34:28 -0600

    update iframe to show only events

--- iframe.js
+++ iframe.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-    let smallsrc = "https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FHoustonSpaceCityzens%2F&tabs=timeline&width=300&height=345&small_header=true&adapt_container_width=false&hide_cover=true&show_facepile=false&appId"
-    let largesrc = "https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FHoustonSpaceCityzens%2F&tabs=timeline&width=500&height=280&small_header=true&adapt_container_width=true&hide_cover=true&show_facepile=true&appId"
+    let smallsrc = "https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FHoustonSpaceCityzens%2F&tabs=events&width=300&height=430&small_header=true&adapt_container_width=false&hide_cover=true&show_facepile=false&appId"
+    let largesrc = "https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FHoustonSpaceCityzens%2F&tabs=events&width=500&height=550&small_header=true&adapt_container_width=false&hide_cover=true&show_facepile=false&appId"
         if(window.outerWidth < 550){
             $('#iframesrc').attr('src', smallsrc);
         } else{
